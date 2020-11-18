10f721977146caa3a0463931291157c24cb137fb
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2020-11-18 11:55:59 -0500

    Background picture up on landing page.

--- app/javascript/react/components/LandingContainer.js
+++ app/javascript/react/components/LandingContainer.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import TopBar from './TopBar'
-
+import '../../../assets/stylesheets/main.scss';
 const LandingContainer = (props) => {
   return(
     <div>
       <TopBar/>
+      <div className='backgroundLanding '>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='personalinfobox col-sm'>
+              <div className='titlebar'>
+                <h1>Contact Information</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
