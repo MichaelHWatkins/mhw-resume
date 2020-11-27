d6a99268bf7a5c72cbf95d5b765220abd69b9420
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2020-11-27 13:41:59 -0500

    Added in some stuff.

--- app/javascript/react/components/ProjectsContainer.js
+++ app/javascript/react/components/ProjectsContainer.js
@@ -11,16 +11,18 @@ const ProjectsContainer = (props) => {
           <div className='bgproj1 col-xs-12 col-lg-3'>
             <h4>Cambridge Eats</h4>
             <p>This project was built in two weeks. It was designed to give users a website to review restaurants in Cambridge, MA. Much like yelp you can sign in, write reviews of restaurants, as well as add new restaurants.</p>
-            <a href="https://github.com/veeveeanne/restaurants_in_cambridge"><img className="gitcat" src={gitcat}/></a>
+            <p><a href="https://github.com/veeveeanne/restaurants_in_cambridge"><img className="gitcat" src={gitcat}/></a></p>
+            <p className="center"><a href="http://cambridge-eats.herokuapp.com/">Cambridge Eats</a></p>
           </div>
           <div className='bgproj2 col-xs-12 col-lg-3'>
             <h4>Fantasy Portfolio</h4>
             <p>This Project had a two week timeline. It was designed to give users an experience like a fantasy football leauge, only exchanging football teams for stocks on the stock exchange.</p>
             <a href="https://github.com/MichaelHWatkins/fantasy_portfolio"><img className="gitcat" src={gitcat}/></a>
+            <p className="center"><a href="http://fantasy-portfolio-league.herokuapp.com/">Fantasy Portfolio Leauge</a></p>
           </div>
           <div className='bgproj3 col-xs-12 col-lg-3'>
             <h4>My Resume Website</h4>
-            <p>This is what you are currently viewing. It was designed to put together all my job related information as well as give a sample of what I am able to do. It is mainly created using a frontend skillset.</p>
+            <p>This is what you are currently viewing. It was designed to put together all my job related information as well as give a sample of what I am able to do. It is mainly created using http and css.</p>
             <a href="https://github.com/MichaelHWatkins/mhw-resume"><img className="gitcat" src={gitcat}/></a>
           </div>
         </div>
